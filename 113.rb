3689a4e60413de3baa6f2788fcf29134081e5272
Cristian B <cris.bahena77@gmail.com>
2023-10-23 14:16:01 -0500

    Update 113.rb

---
+++ 113.rb
@@ -0,0 +1,22 @@
+# QUESTION 1
+# Write code to store 3 different numbers in three different variables.
+# Then write the code to print the three variables added together.
+
+one = 3
+two = 7
+three = 1
+puts one + two + three
+
+# QUESTION 2
+# If you run the code below, it will crash due to invalid variable names. 
+# Find the invalid variable names and delete the special characters 
+# (be sure to leave the special characters in the valid variable names alone).
+
+bob7 = 50
+bob = 50
+bob = 50
+@bob = 50
+bob_ = 50
+_bob = 50
+bob = 50
+$bob = 50
