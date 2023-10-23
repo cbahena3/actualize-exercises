9d835e87743e4dd55fc68a79bfed02d6dba7d56a
Cristian B <cris.bahena77@gmail.com>
2023-10-23 13:21:52 -0500

    Create 111.rb

---
+++ 111.rb
@@ -0,0 +1,21 @@
+# QUESTION 1
+# Write code to print out the numbers 4, 30, 12, 983 on separate lines. 
+# For this exercise, you are required to use addition, subtraction, multiplication, and division at least once.
+
+puts 2 + 2
+puts 300 - 270
+puts 3 * 4
+puts 1966 / 2
+
+
+
+
+# QUESTION 2
+# Write code using addition, subtraction, multiplication, or division with numbers that causes the computer to crash.
+# Add a comment with a copy of the error message.
+# Add a comment that describes the error message in your own words.
+
+puts 0/0  
+
+#Error: 111.rb:18:in `/': divided by 0 (ZeroDivisionError) from 111.rb:18:in `<main>'
+# The computer recognises that a number cannot be divded by 0 so it says 'divided by 0' on line 18.
