24751573bd131983f6d4eda08e39440a8fab813e
Cristian B <cris.bahena77@gmail.com>
2023-10-24 09:51:39 -0500

    Create calc.rb

---
+++ calc.rb
@@ -0,0 +1,24 @@
+#floats
+puts 1.0 + 2.0 
+puts 2.0 * 3.0
+puts 5.0 - 8.0
+puts 9.0 / 4.5
+
+#integers
+puts 1+2
+puts 2*3
+puts 5-8
+puts 9/2
+
+#strings
+puts 'Hello, world!'
+puts ''
+puts 'Good-bye.'
+
+#backslash \
+puts 'you\'re swell'
+puts 'the \ cow'
+
+#variables
+myStaff = 'myself and mr.banana!'
+puts 'My staff is ' + myStaff
