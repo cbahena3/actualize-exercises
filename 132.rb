34ce92711826a5b0b8d3a9f1a40355ce4640b34e
Cristian B <cris.bahena77@gmail.com>
2023-10-25 10:20:37 -0500

    Update 132.rb

--- 132.rb
+++ 132.rb
@@ -3,22 +3,22 @@
 # Then run the code to see if the output matches your annotation.
 word = "hi"           # 1
 index = 0             # 2
-while index < 2       # 
-  word = word + word  # 
-  index = index + 1   # 
+while index < 2       # 3 6 9
+  word = word + word  # 4 7
+  index = index + 1   # 5 8
 end
-p word                # 
+p word                # 10
 
 # 1: word is "hi"
 # 2: word is "hi", index is 0
-# 3: 
-# 4:
-# 5: 
-# 6: 
-# 7: 
-# 8: 
-# 9: 
-# 10: 
+# 3: index is less than 2, continue
+# 4: word= hihi 
+# 5: index increases to 1
+# 6: index is less than 2, continue
+# 7: word= hihihihi
+# 8: index increases to 2
+# 9: index is not less than 2, end loop
+# 10: prints hihihihi
 
 
 # QUESTION 2
@@ -26,11 +26,11 @@ p word                #
 # Then run the code to see if the output matches your annotation.
 index = 0             # 1
 while index > 2       # 2
-  puts index          # 
-  index = index + 1   # 
+  puts index          # does not execute
+  index = index + 1   # does not execute
 end 
-puts "Done!"          # 
+puts "Done!"          # 3
 
-# 1: 
-# 2: 
-# 3: 
+# 1: index is 0
+# 2: index is not greater than 2, loop never starts
+# 3: 'Done!' is printed
