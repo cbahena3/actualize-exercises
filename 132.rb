f74fe120b51959ae17bfecb5d10de1f0ff0d805d
Cristian B <cris.bahena77@gmail.com>
2023-10-25 10:20:06 -0500

    Create 132.rb

---
+++ 132.rb
@@ -0,0 +1,36 @@
+# QUESTION 1
+# Finish annotating the code below.
+# Then run the code to see if the output matches your annotation.
+word = "hi"           # 1
+index = 0             # 2
+while index < 2       # 
+  word = word + word  # 
+  index = index + 1   # 
+end
+p word                # 
+
+# 1: word is "hi"
+# 2: word is "hi", index is 0
+# 3: 
+# 4:
+# 5: 
+# 6: 
+# 7: 
+# 8: 
+# 9: 
+# 10: 
+
+
+# QUESTION 2
+# Finish annotating the code below.
+# Then run the code to see if the output matches your annotation.
+index = 0             # 1
+while index > 2       # 2
+  puts index          # 
+  index = index + 1   # 
+end 
+puts "Done!"          # 
+
+# 1: 
+# 2: 
+# 3: 
