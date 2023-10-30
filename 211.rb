3ad5c39c83ad340b11b2fd299946ff7e5dd8c377
Cristian B <cris.bahena77@gmail.com>
2023-10-30 14:56:20 -0500

    practice exercies

---
+++ 211.rb
@@ -0,0 +1,17 @@
+# DELIBERATE PRACTICE REFLECTION
+# Paste a deliberate practice question you are currently working on below.
+# Write the code to solve the problem without looking at any notes or other resources.
+
+#1. Write a program that uses a variable to store a number,
+#then prints that number plus 10.
+
+bob = 215
+puts bob + 10
+
+# Were you able to easily solve the problem from memory?
+# If so, describe the next skill you will start to practice tomorrow.
+# If not, describe the part you got stuck on that requires more practice.
+
+#I was able to easily solve this from memory.
+#I will try a few more variable exercises first
+#and tomorrow move onto conditionals.
