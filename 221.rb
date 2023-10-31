d2051e3ab1a737dcc9a330e68195d073919752fc
Cristian B <cris.bahena77@gmail.com>
2023-10-31 10:53:52 -0500

    create 221.rb

---
+++ 221.rb
@@ -0,0 +1,15 @@
+# DELIBERATE PRACTICE REFLECTION
+# Paste a deliberate practice question you are currently working on below.
+# Write the code to solve the problem without looking at any notes or other resources.
+#Write a while loop to print the numbers 1 through 10.
+number = 1
+while number <= 10
+    puts number
+    number = number + 1
+end
+# Were you able to easily solve the problem from memory?
+# If so, describe the next skill you will start to practice tomorrow.
+# If not, describe the part you got stuck on that requires more practice.
+
+#I was able to easily solve it from memory so tomorrow I will work on 
+#different questins related to loops.
