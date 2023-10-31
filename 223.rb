c3a5f11183bab601668b4ac2972ae904a80ff16e
Cristian B <cris.bahena77@gmail.com>
2023-10-31 12:09:41 -0500

    Correct now

--- 223.rb
+++ 223.rb
@@ -2,11 +2,11 @@
 # Annotate the following code to keep track of the variables.
 x = 10                # 1
 y = 4                 # 2
-if x == 10 && y == 10 
-  x = 10              # 3
-  y = 10              # 4
+if x == 10 && y == 10 # 3
+  x = 10              
+  y = 10              
 end                   
-if x == 10 || y == 10 
+if x == 10 || y == 10 # 4
   x = x + y           # 5
   y = x + y           # 6
 end                   
@@ -15,10 +15,10 @@ p y                   # 8
 
 # 1: x is 10
 # 2: x is 10, y is 4
-# 3: if x and y both are 10 then x is 10
-# 4: if x and y both are 10 then y is 10
-# 5: if x or y is 10 then x is x+y, in this case 14
-# 6: if x or y is 10 then y is x+y, in this case 18
+# 3: checks if x and y are both 10, they are not so doesn't execute
+# 4: checks if x or y is 10, executes since x is 10
+# 5: x is 14
+# 6: y is 18 because x= 14, y was 4
 # 7: prints 14
 # 8: prints 18
 
