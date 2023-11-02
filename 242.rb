0e788a7f03ee3dd0cbd59f277dd1d1038d2e18f2
Cristian B <cris.bahena77@gmail.com>
2023-11-02 14:33:53 -0500

    create 242.rb

---
+++ 242.rb
@@ -0,0 +1,15 @@
+# QUESTION 1
+# Use a variable to store an array with three different words.
+# Then add one more word to the array and print the array on one line.
+
+myArray = ['pizza', 'soda', 'popcorn']
+myArray.push('ice cream')
+pp myArray
+
+# QUESTION 2
+# Use a variable to store a hash with two different words and their definitions.
+# Then add one more word with its definition and print the hash on one line.
+
+sport = {'soccer' => 'soccer ball', 'tennis' => 'tennis ball', 'golf' => 'golf ball'}
+sport ['track'] = 'running'
+puts sport
