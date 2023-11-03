d57b3efbce650c9ad79c097fcc90527f99fc5107
Cristian B <cris.bahena77@gmail.com>
2023-11-03 09:09:38 -0500

    create 251.rb

---
+++ 251.rb
@@ -0,0 +1,16 @@
+# DELIBERATE PRACTICE REFLECTION
+# Paste a deliberate practice question you are currently working on below.
+# Write the code to solve the problem without looking at any notes or other resources.
+# Create an array to store 3 words. 
+# Then add two more words to the array and print the array on one line.
+myArray = ["Banana", "Apple", "Orange"]
+pp myArray
+myArray.push("Mango", "Pineapple")
+pp myArray
+
+
+# Were you able to easily solve the problem from memory?
+# If so, describe the next skill you will start to practice tomorrow.
+# If not, describe the part you got stuck on that requires more practice.
+# I was able to solve it pretty easy, I will practice harder array problems.
+
