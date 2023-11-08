7b1806812c37879e0cea02855b63b60bc74d4563
Cristian B <cris.bahena77@gmail.com>
2023-11-07 18:19:40 -0600

    create 312.rb

---
+++ 312.rb
@@ -0,0 +1,19 @@
+# QUESTION 1
+# Use a loop to print out only the numbers greater than 10.
+
+# numbers = [4, 53, 22, 3, 9, 7, 12]
+# #create a loop that outputs numbers (practicing)
+# # index = 0
+# # while index < numbers.length
+# #     puts numbers[index]
+# #     index = index + 1
+# # end
+
+numbers = [4, 53, 22, 3, 9, 7, 12]
+index = 0                           
+while index < numbers.length        
+    if numbers[index] > 10
+        puts numbers[index]
+    end
+    index = index + 1
+end
