dcb4b25bac78beea6365804ec7a617cfbe1167d5
Cristian B <cris.bahena77@gmail.com>
2023-11-07 18:03:23 -0600

    create 311.rb

---
+++ 311.rb
@@ -0,0 +1,22 @@
+# DELIBERATE PRACTICE REFLECTION
+# Paste a deliberate practice question you are currently working on below.
+# Write the code to solve the problem without looking at any notes or other resources.
+
+#Start with an array of numbers and create a new array with each number times 3.
+#For example, [1, 2, 3] becomes [3, 6, 9].
+arrayOne = [10, 20, 30]
+arrayTwo = []
+index = 0
+while index < arrayOne.length
+    arrayTwo.push(arrayOne[index] * 3)
+    index = index + 1
+end
+pp arrayTwo
+
+# Were you able to easily solve the problem from memory?
+# If so, describe the next skill you will start to practice tomorrow.
+# If not, describe the part you got stuck on that requires more practice.
+# at first I had trouble because I was not sure how to
+# make the loop add the numbers to my second array
+# I need more practice on methods. 
+
