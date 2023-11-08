01fa50dbc057e18a1ebfbcd7c20af4807a34a919
Cristian B <cris.bahena77@gmail.com>
2023-11-08 09:59:57 -0600

    Add exercise solutions

---
+++ 323.rb
@@ -0,0 +1,9 @@
+# QUESTION 1
+# Create a program that multiplies by 2 until 100,000. 
+# That is, the program should print out: 1, 2, 4, 8, 16, 32, etc. until you get above 100,000.
+
+hundredThousand = 1
+while hundredThousand <= 100000
+    puts hundredThousand
+    hundredThousand = hundredThousand * 2
+end
