5e01df11095818534f5a4b0c80e1b4dd9c9606cb
Cristian B <cris.bahena77@gmail.com>
2023-11-08 09:22:49 -0600

    create 321.rb

---
+++ 321.rb
@@ -0,0 +1,17 @@
+# DELIBERATE PRACTICE REFLECTION
+# Paste a deliberate practice question you are currently working on below.
+# Write the code to solve the problem without looking at any notes or other resources.
+# Write a method that takes in a number and returns the number times two. 
+# Then run the method and print the result.
+def double(number)
+    return number * 2
+end
+puts double(100)
+
+
+# Were you able to easily solve the problem from memory?
+# If so, describe the next skill you will start to practice tomorrow.
+# If not, describe the part you got stuck on that requires more practice.
+# I did not find it hard I just realized I forgot the parameters 
+# I need to practice more with custom methods to remember all thats needed
+
