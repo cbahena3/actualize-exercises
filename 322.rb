f1c92f7c37d9a3ed3cba4d02e831d49569dcb486
Cristian B <cris.bahena77@gmail.com>
2023-11-09 12:00:33 -0600

    update 322.rb

--- 322.rb
+++ 322.rb
@@ -6,8 +6,12 @@ dogs = [
   { name: "Beethoven", breed: "Saint Bernard" }
 ]
 index = 0
+array = []
 while index < dogs.length
-    array = [dogs[index][:name]]
-    pp array
+    array.push(dogs[index][:name])
     index = index + 1
 end
+pp array
+
+#I believe this is what you meant? I created the array and pushed the names
+#to the new array and called it so it printed the array
