59726cdd7a50421823ad20ceaab6d5af296fdedb
Cristian B <cris.bahena77@gmail.com>
2023-11-09 20:24:31 -0600

    Add exercise solutions

---
+++ 341.rb
@@ -0,0 +1,16 @@
+# DELIBERATE PRACTICE REFLECTION
+# Paste a deliberate practice question you are currently working on below.
+# Write the code to solve the problem without looking at any notes or other resources.
+# Write a method that takes in two numbers and
+# returns the first number subtracted by the second.
+# Then run the method and print the result.
+def theNums(first, second)
+  return first - second
+end
+puts theNums(10, 3)
+
+# Were you able to easily solve the problem from memory?
+# If so, describe the next skill you will start to practice tomorrow.
+# If not, describe the part you got stuck on that requires more practice.
+# I was able to piece the answer together from memory.
+# I will take on harder custom methods.
