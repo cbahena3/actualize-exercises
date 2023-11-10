0dfe0eef0a3dd1d51bb51ed5f408049f6a50a7ac
Cristian B <cris.bahena77@gmail.com>
2023-11-10 10:31:25 -0600

    Add exercise solutions

--- 334.rb
+++ 334.rb
@@ -7,8 +7,39 @@
 # After the user is done, the program will tell the user
 # which word was entered the most frequently.
 # For example, if the user enters:
-#   apple, banana, orange, pear, apple, pear, apple, squash, apple, pear
+# apple, banana, orange, pear, apple, pear, apple, squash, apple, pear
 # The program will say: "apple was your most common word"
 # (That's because apple appeared in the user's list 4 times, more than any other word.)
 
 
+# #get 5 words from user
+
+# 5.times do
+#   puts "Enter a word:"
+#   word = gets.chomp
+# end
+
+# # make array for word
+# wordList = []
+# 5.times do
+#   puts "Enter a word:"
+#   word = gets.chomp
+#   wordList << word
+# end
+# pp wordList
+
+# # loop through wordList
+# index = 0
+# while index < wordList.length
+#   pp wordList[index]
+# index = index + 1
+# end
+
+#create a count for each word
+wordCount = Hash.new(0)
+10.times do
+  puts "Enter your word:"
+  user = gets.chomp
+  wordCount[user] +=1
+end
+pp wordCount
