af877d68a3f37f55560e0f70d11fad2de256b7f7
Cristian B <cris.bahena77@gmail.com>
2023-11-13 14:22:59 -0600

    create 404.rb

---
+++ 404.rb
@@ -0,0 +1,22 @@
+# QUESTION 1
+
+# 1) I will be choosing schedule "Version 4 - Daytime Daily Habits Sprint"
+# 2) I believe this will work best for me because I only have a part-time job right now.
+# I also want to work on building projects to better my programming. Dedicating a good amount
+# of time to algorithms and deliberate practice is also something I want to do. And of course
+# applications and networking is important as well.
+
+
+# If you do have a full-time job or other daytime commitments:
+#     Make a copy of the Job Search Schedules - Evenings/Weekends Google Document.
+#     https://docs.google.com/document/d/1dlSQcCimFHJqcyJggmeZWv4vmBnbos-zNsH8vdeA_D8/copy
+# If you do not have a full-time job or other daytime commitments:
+#     Make a copy of the Job Search Schedules - Daytime Google Document.
+#     https://docs.google.com/document/d/1YpdKGuinjqcNLcER5r4mAtkQYZ0tWdtZoor83wSUuIw/copy
+# At the top of the document, write answers to the following questions:
+#     Which schedule version are you choosing?
+#     Why do you feel it works best for you?
+# Change the sharing settings of the document to "Anyone with the link can view"
+# Copy the link to your Job Search Schedules document and paste it below.
+
+# LINK: https://docs.google.com/document/d/1XoLO1P4sxQnthxoub75gpJI63LwRF43jM-q8OZ7CIo4/edit?usp=sharing
