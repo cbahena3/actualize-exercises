3ab182d1554d6a6c9fb6b6e90424883a90157240
Cristian B <cris.bahena77@gmail.com>
2023-11-15 10:35:31 -0600

    updated 422.rb

--- 422.rb
+++ 422.rb
@@ -3,7 +3,12 @@
 # and returns the string repeated n times.
 # Then run the method and print the result.
 
-def myString(apple = 'Apple', n = 2)
-  return apple.to_s * n
+def myString (word, num)
+  if word.is_a?(String) && num.is_a?(Integer)
+    return word * num
+  else
+    puts "Error: Word and/or number not detected."
+  end
 end
-puts myString("hi", 12)
+
+puts myString('1', 3)
