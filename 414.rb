0f800d91925bbaf0a56327fbea59cc6ac0f7100b
Cristian B <cris.bahena77@gmail.com>
2023-11-14 20:05:14 -0600

    Add exercise solutions

--- 414.rb
+++ 414.rb
@@ -4,3 +4,12 @@
 # sentence "This is an example!" in it.
 
 
+File.new('example.txt', File::RDWR | File::CREAT | File::EXCL)
+sentence = 'This is an example!'
+File.open('example.txt', File::RDWR) do |file|
+  file.puts sentence
+end
+
+puts "File 'example.txt' created successfully with the sentence: '#{sentence}'"
+#I sort of understand it by using file ruby api reading and a mix of chat gpt reading
+#I do not feel too confident in this yet
