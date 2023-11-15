5e4315fbb37f59384a8e035082d0ae8aa1ff5147
Cristian B <cris.bahena77@gmail.com>
2023-11-15 08:21:59 -0600

    create 431.br

---
+++ 431.rb
@@ -0,0 +1,22 @@
+# DELIBERATE PRACTICE REFLECTION
+# Paste a deliberate practice question you are currently working on below.
+# Write the code to solve the problem without looking at any notes or other resources.
+# Start with an array of numbers and compute the sum of all the numbers.
+# For example, [5, 10, 8, 3] becomes 26
+array = [100, 200, 300, 400, 500, 600, 700]
+index = 0
+total = 0
+while index < array.length
+  total = total + array[index]
+index = index + 1
+end
+puts total
+
+
+
+
+# Were you able to easily solve the problem from memory?
+# If so, describe the next skill you will start to practice tomorrow.
+# If not, describe the part you got stuck on that requires more practice.
+#It was not difficult but I did not do the each or reduce methods which would
+#make it shorter. I will practice more loops to figure out quicker ways to code
