660cc52286352bbb47c1d3d12951d832f8fd93dc
Cristian B <cris.bahena77@gmail.com>
2023-11-14 20:12:19 -0600

    Add exercise solutions

---
+++ 421.rb
@@ -0,0 +1,18 @@
+# DELIBERATE PRACTICE REFLECTION
+# Paste a deliberate practice question you are currently working on below.
+# Write the code to solve the problem without looking at any notes or other resources.
+
+# Make an array of hashes to store the title and author for 3 different books.
+# Then print out the third book's author.
+books = [
+  {"Dog Days" => "Jeff Kinney"},
+  {"The Hunger Games" => "Suzanne Collins"},
+  {"The Wolf of Wall Street" => "Jordan Belfort"}
+]
+puts books [2]["The Wolf of Wall Street"]
+
+# Were you able to easily solve the problem from memory?
+# If so, describe the next skill you will start to practice tomorrow.
+# If not, describe the part you got stuck on that requires more practice.
+
+# Overall I thought it was a simple exercise but I will continue to practice hard ones.
