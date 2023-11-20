794126b885f6e33199ec49f56606046fd05107e9
Cristian B <cris.bahena77@gmail.com>
2023-11-19 18:20:37 -0600

    Add exercise solutions

---
+++ 521.rb
@@ -0,0 +1,17 @@
+# DELIBERATE PRACTICE REFLECTION
+# Paste a deliberate practice question you are currently working on below.
+# Write the code to solve the problem without looking at any notes or other resources.
+
+# Make a hash to store a shirt's brand, color, and size.
+# Then print each attribute on separate lines.
+
+myShirt = {brand: "H&M", color: "Blue", size: "Medium"}
+pp myShirt[:brand]
+pp myShirt[:color]
+pp myShirt[:size]
+
+# Were you able to easily solve the problem from memory?
+# If so, describe the next skill you will start to practice tomorrow.
+# If not, describe the part you got stuck on that requires more practice.
+# this exercise was easy and I was able to remember how to do it from memory.
+# I will continue to practice more challenging hashes.
